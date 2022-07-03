54bbd341d38475b6e416ef1f6a047bb791a25554
Eric Efrain <87373625+EricAlmendras@users.noreply.github.com>
2022-07-03 17:22:16 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+// const miTitulo = document.querySelector('h1');
+// miTitulo.textContent = 'Hola Mozilla'
+
+// document.querySelector('html').onclick = function() {
+//     alert('¡Ouch! ¡Deja de pincharme!');
+// };
+
+let miImage = document.querySelector('img');
+miImage.onclick = function() {   
+    let src = miImage.getAttribute('src');
+
+    if (src === 'images/firefox.png'){
+        miImage.setAttribute('src','images/firefox2.png');
+    } else {
+        miImage.setAttribute('src','images/firefox.png');
+    }
+}
+
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if(!miNombre) {
+      estableceNombreUsuario();
+    } else {
+      localStorage.setItem('nombre', miNombre);
+      miTitulo.innerHTML = 'Galeria de logos de: ' + miNombre;
+    }
+  }
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
